a2a017424f938fcea871810dc17190022cf5b731
dongmin1011 <82290372+dongmin1011@users.noreply.github.com>
2023-10-25 13:47:21 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -25,14 +25,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
